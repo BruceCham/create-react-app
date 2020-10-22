482d6ec465b18ed49cc17a89bdccad4c5f920ee1
zhanglitao1 <zhanglitao1@100tal.com>
2020-10-22 17:37:03 +0800

    feat: 增加 px to rem

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,18 @@
-module.exports = function override(config, env) {
-  //do stuff with the webpack config...
-  return config;
-}
+const { override, addPostcssPlugins } = require('customize-cra');
+
+module.exports = override(
+  addPostcssPlugins([require('postcss-pxtorem')({
+    rootValue: 100,
+    unitPrecision: 5,
+    mediaQuery: false,
+    minPixelValue: 0,
+    propList: [
+      '*background*', '*padding*', '*margin*',
+      'letter-spacing', '*width', '*height',
+      'left', 'right', 'top', 'bottom', 'font*',
+      'border-radius',
+    ],
+  }),]),
+  (config, env) => {
+    return config;
+  })
